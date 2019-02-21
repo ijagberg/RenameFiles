549269246ca3a3be7d67cede431c2dbedb67e753
Isak Jägberg <ijagberg@gmail.com>
2019-02-21 14:29:05 +0100

    Seems to be working, wouldn't call it stable though

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate clap;
+extern crate uuid;
 use clap::{App, Arg};
 use std::fs;
+use std::path::PathBuf;
+use std::result::Result;
+use uuid::Uuid;
 
 // program
-//     .option('-g, --guid', 'Use the GUID naming convention (####-####-####-####, ...). This is the default naming convention used.')
-//     .option('-i, --incremental', 'Use the incremental naming convention (1, 2, 3, ...)')
 //     .option('-s, --suppress', 'Suppress prompt for confirmation')
 //     .parse(process.argv);
 
@@ -13,6 +15,7 @@ struct Config {
 }
 
 fn main() {
+    // handle command line parsing
     let matches = App::new("RenameFiles")
         .version("1.0")
         .author("Isak Jägberg <ijagberg@gmail.com>")
@@ -22,11 +25,63 @@ fn main() {
                 .short("s")
                 .help("Suppress prompt for confirmation"),
         )
+        .arg(Arg::with_name("files").required(true).min_values(1))
         .get_matches();
 
-    let config = Config{
-        suppress: matches.is_present("suppress")
+    let config = Config {
+        suppress: matches.is_present("suppress"),
     };
+    let files: Vec<&str> = matches.values_of("files").unwrap().collect();
 
-    
+    run(config, files);
+}
+
+fn run(config: Config, files: Vec<&str>) {
+    if !config.suppress {
+        // print confirmation
+        println!("This will be a confirmation eventually");
+        return;
+    }
+
+    rename_files(config, files);
+}
+
+fn rename_files(_config: Config, files: Vec<&str>) {
+    for file in files {
+        if file.starts_with('.') {
+            continue;
+        }
+        // rename single file
+        let metadata = match fs::metadata(file) {
+            Ok(metadata) => metadata,
+            Err(e) => {
+                eprintln!("{}", e);
+                return;
+            }
+        };
+        if metadata.is_file() {
+            match rename_file(PathBuf::from(file)) {
+                Ok(()) => {}
+                Err(e) => eprintln!("Error for file {}: {}", file, e),
+            }
+        }
+    }
+}
+
+fn rename_file(current_path: PathBuf) -> Result<(), std::io::Error> {
+    let random_name = Uuid::new_v4().to_string();
+    let mut new_path = PathBuf::from(current_path.parent().unwrap());
+    new_path.push(random_name);
+    new_path.set_extension(current_path.extension().unwrap());
+    match fs::rename(&current_path, new_path.clone()) {
+        Ok(()) => {
+            println!(
+                "{} -> {}",
+                current_path.to_str().unwrap(),
+                new_path.to_str().unwrap()
+            );
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
 }
